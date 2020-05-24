1cfcdd33e37b8ac616c72f3820b25d90bcb7d054
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-05-24 21:04:36 +0200

    added GET/id

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,22 @@ const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE ||'database.sqlite');
 
+artistsRouter.param('artistId', (req, res, next, artistId) => {
+    db.get(`select *
+            from main.Artist
+            where Artist.id = $artistId`,
+        {$artistId: artistId}, (err, artist) => {
+            if (err) {
+                next(err);
+            } else if (artist) {
+                req.artist = artist;
+                next();
+            } else {
+                res.sendStatus(404);
+            }
+        });
+});
+
 artistsRouter.get('/', (req, res, next) => {
     db.all(`select * from Artist where Artist.is_currently_employed = 1`,
         (err, artists) => {
@@ -14,5 +30,9 @@ artistsRouter.get('/', (req, res, next) => {
             }
     });
 });
+
+artistsRouter.get('/:artistId', ((req, res, next) => {
+    res.status(200).json({artist: req.artist});
+}))
 
 module.exports = artistsRouter;
